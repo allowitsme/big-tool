5ecb9d7b2bc7f1c1302d380a5400ab45a532a3d4
Dmitry Frolov <40766800+allowitsme@users.noreply.github.com>
2020-11-28 11:24:45 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,146 @@
-# big-tool
-Coming November, 28. NO HAT 2020.
+# BIOS Guard Script tool
+
+The tool allows you to assemble and disassemble BIOS Guard script.
+
+## Assembly
+
+To assemble a script, you need to use `big-script-tool.py -a <script_file>`.
+Assembled script will be saved to file `<script_file>_assembled.bin`.
+
+Also you can use class `BigScript` directly:
+```
+script = BigScript(code_string=src)
+script.to_bytes()
+```
+or
+```
+script = BigScript()
+script.assemble(src)
+script.to_bytes()
+```
+
+### Operands
+
+The operands are:
+* buffers
+* flash pointers
+* registers
+* immediate values
+* labels
+
+**Buffers**: `buffer<number_of_the_buffer>` (or `b<number_of_the_buffer>`). Storages of the data.
+`buffer0` points to the actual BGUP. Other buffers are temporal.
+```
+set b0 0xAA00
+add b0 0x1
+```
+
+**Flash pointers**: `flash<number_of_the_flash_ptr>` (or `f<number_of_the_flash_ptr>`). Stores some linear address of the flash.
+```
+set f0 0xA0000000
+sub f0 0x10
+```
+**Registers**: `r<number_of_the_register>` - a little storage of the temporal data.
+```
+set r1 0x10
+set r2 0x20
+add r1 r2
+and r1 0x30
+```
+**Imm values**: `<number>` or `0x<number>` if you prefer to use hex. ***Please note***, that hex numbers are start only with `0x` prefix. If you write something like `10000h` it will be interpreted as a decimal number.
+
+**Labels**: `_<name_of_the_label>` - an arbitrary script line name. Can be used only in control flow instructions.
+***Please note***, that labels start with `_` and must not end with symbols like `:`. If you use that symbol, you will need to refer to the label like `jmp _label:`.
+```
+_loop
+  sub r1 r2
+  cmp r1 0x0
+  jne _loop
+```
+
+### Opcodes
+
+| Opcode name | Operands | Info |
+|-|-|-|
+| start | - | marks the start of the script |
+| store | `flash ptr, buffer, register` | write data from a buffer to the flash |
+| store | `flash ptr, buffer, immediate value` | write data from a buffer to the flash |
+| load | `buffer, flash ptr, register` | read data from the flash to the buffer |
+| load | `buffer, flash ptr, immediate value` | read data from the flash to the buffer |
+| eraseblk | `flash ptr` | Erase flash blk |
+| eraseblk64kb | `flash ptr` | Erase 64kb flash block |
+| add | `register, register` | |
+| add | `register, immediate value` | |
+| add | `buffer, register` | |
+| add | `buffer, immediate value` | |
+| add | `flash ptr, register` | |
+| add | `flash ptr, immediate value` | |
+| sub | `register, register` | |
+| sub | `register, immediate value` | |
+| sub | `buffer, register` | |
+| sub | `buffer, immediate value` | |
+| sub | `flash ptr, register` | |
+| sub | `flash ptr, immediate value` | |
+| and | `register, register` | |
+| and | `register, immediate value` | |
+| or | `register, register` | |
+| or | `register, immediate value` | |
+| shr | `register, immediate value` | |
+| shl | `register, immediate value` | |
+| ror | `register, immediate value` | |
+| rol | `register, immediate value` | |
+| set | `register, register` | |
+| set | `register, immediate value` | |
+| set | `buffer, register` | Set an offset from the beggining of the buffer |
+| set | `buffer, immediate value` | Set an offset from the beggining of the buffer |
+| set | `flash ptr, register` | Set a linear address of the flash |
+| set | `flash ptr, immediate value` | Set a linear address of the flash |
+| loadbyte | `register, buffer` | |
+| loadword | `register, buffer` | |
+| loaddword | `register, buffer` | |
+| storebyte | `buffer, register` | |
+| storeword | `buffer, register` | |
+| storedword | `buffer, register` | |
+| cmp | `register, register` | |
+| cmp | `register, immediate value` | |
+| cmp | `buffer, register` | |
+| cmp | `buffer, immediate value` | |
+| cmp | `flash ptr, register` | |
+| cmp | `flash ptr, immediate value` | |
+| cmp | `buffer, buffer, register` | Compare buffers with specified length |
+| cmp | `buffer, buffer, immediate value` | Compare buffers with specified length |
+| copy | `buffer, buffer, register` | Copy data with the specified length |
+| copy | `buffer, buffer, immediate value` | Copy data with the specified length |
+| jmp | `label` (or `imm`) | Uncoditional jump to the label. Also you can use a line number directly. |
+| je | `label` (or `imm`)  | Coditional jump to the label. Also you can use a line number directly.|
+| jne | `label`(or `imm`)  | Coditional jump to the label. Also you can use a line number directly. |
+| jg | `label` (or `imm`)  | Coditional jump to the label. Also you can use a line number directly. |
+| jge | `label` (or `imm`)  | Coditional jump to the label. Also you can use a line number directly. |
+| jl | `label` (or `imm`)  | Coditional jump to the label. Also you can use a line number directly. |
+| jle | `label` (or `imm`)  | Coditional jump to the label. Also you can use a line number directly. |
+| jmp | `register` | Uncoditional jump to the line number stored in register. |
+| log | `immediate value, register` | Write data from second operand to the BiG Log. The BiG log needs to be setup properly. |
+| log | `immediate value, immediate value` | Write data from second operand to the BiG Log. The BiG log needs to be setup properly. |
+| rdsts | `register` | Read status of previous operation into register. |
+| rand | `register` | |
+| sleep | `immediate value` | |
+| end | - | |
+
+
+## Disassembly
+
+To disassemble a script, you need to use `big-script-tool.py -d <script_file>`.
+Disassembled script will be saved to file `<script_file>_disassembled.txt`.
+
+Also you can use class `
+BigScript` directly:
+```
+script = BigScript(code_bytes=bin_script)
+script.to_string()
+```
+or
+```
+script = BigScript()
+script.disassemble(bin_script)
+script.to_string()
+```
---
+++ big-script-tool.py
@@ -0,0 +1,489 @@
+from optparse import OptionParser, make_option
+
+import re
+import struct
+
+INSTR_LEN = 8
+MAX_OPERANDS = 3
+OPCODE_LEN = 2
+
+OP1_NUM = 0
+OP2_NUM = 1
+OP3_NUM = 2
+
+#
+# @TODO: take it out of here (json probably)
+#
+opcodes = {
+    0x01: { 'name': 'start',
+            'operands': ['unused', 'unused', 'unused'] },
+    0x10: { 'name': 'store',
+            'operands': ['flash', 'buff', 'reg'] },
+    0x11: { 'name': 'store',
+            'operands': ['flash', 'buff', 'imm'] },
+    0x12: { 'name': 'load',
+            'operands': ['buff', 'flash', 'reg'] },
+    0x13: { 'name': 'load',
+            'operands': ['buff', 'flash', 'imm'] },
+    0x14: { 'name': 'eraseblk',
+            'operands': ['flash', 'unused', 'unused'] },
+    0x15: { 'name': 'eraseblk64kb',
+            'operands': ['flash', 'unused', 'unused'] },
+    0x20: { 'name': 'eccmd1',
+            'operands': ['reg', 'unused', 'unused'] },
+    0x21: { 'name': 'eccmd2',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x22: { 'name': 'eccmd3',
+            'operands': ['reg', 'unused', 'unused'] },
+    0x23: { 'name': 'eccmd4',
+            'operands': ['reg', 'unused', 'unused'] },
+    0x24: { 'name': 'eccmd5',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x25: { 'name': 'eccmd6',
+            'operands': ['reg', 'unused', 'unused'] },
+    0x30: { 'name': 'add',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x31: { 'name': 'add',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x32: { 'name': 'add',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x33: { 'name': 'add',
+            'operands': ['buff', 'unused', 'imm'] },
+    0x34: { 'name': 'add',
+            'operands': ['flash', 'reg', 'unused'] },
+    0x35: { 'name': 'add',
+            'operands': ['flash', 'unused', 'imm'] },
+    0x36: { 'name': 'sub',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x37: { 'name': 'sub',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x38: { 'name': 'sub',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x39: { 'name': 'sub',
+            'operands': ['buff', 'unused', 'imm'] },
+    0x3a: { 'name': 'sub',
+            'operands': ['flash', 'reg', 'unused'] },
+    0x3b: { 'name': 'sub',
+            'operands': ['flash', 'unused', 'imm'] },
+    0x40: { 'name': 'and',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x41: { 'name': 'and',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x42: { 'name': 'or',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x43: { 'name': 'or',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x44: { 'name': 'shr',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x45: { 'name': 'shl',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x46: { 'name': 'ror',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x47: { 'name': 'rol',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x50: { 'name': 'set',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x51: { 'name': 'set',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x52: { 'name': 'set',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x53: { 'name': 'set',
+            'operands': ['buff', 'unused', 'imm'] },
+    0x54: { 'name': 'set',
+            'operands': ['flash', 'reg', 'unused'] },
+    0x55: { 'name': 'set',
+            'operands': ['flash', 'unused', 'imm'] },
+    0x60: { 'name': 'loadbyte',
+            'operands': ['reg', 'buff', 'unused'] },
+    0x61: { 'name': 'loadword',
+            'operands': ['reg', 'buff', 'unused'] },
+    0x62: { 'name': 'loaddword',
+            'operands': ['reg', 'buff', 'unused'] },
+    0x63: { 'name': 'storebyte',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x64: { 'name': 'storeword',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x65: { 'name': 'storedword',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x70: { 'name': 'cmp',
+            'operands': ['reg', 'reg', 'unused'] },
+    0x71: { 'name': 'cmp',
+            'operands': ['reg', 'unused', 'imm'] },
+    0x72: { 'name': 'cmp',
+            'operands': ['buff', 'reg', 'unused'] },
+    0x73: { 'name': 'cmp',
+            'operands': ['buff', 'unused', 'imm'] },
+    0x74: { 'name': 'cmp',
+            'operands': ['flash', 'reg', 'unused'] },
+    0x75: { 'name': 'cmp',
+            'operands': ['flash', 'unused', 'imm'] },
+    0x76: { 'name': 'cmp',
+            'operands': ['buff', 'buff', 'reg'] },
+    0x77: { 'name': 'cmp',
+            'operands': ['buff', 'buff', 'imm'] },
+    0x80: { 'name': 'copy',
+            'operands': ['buff', 'buff', 'reg'] },
+    0x81: { 'name': 'copy',
+            'operands': ['buff', 'buff', 'imm'] },
+    0x90: { 'name': 'jmp',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x91: { 'name': 'je',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x92: { 'name': 'jne',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x93: { 'name': 'jg',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x94: { 'name': 'jge',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x95: { 'name': 'jl',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x96: { 'name': 'jle',
+            'operands': ['unused', 'unused', 'imm'] },
+    0x97: { 'name': 'jmp',
+            'operands': ['reg', 'unused', 'unused'] },
+    0xa0: { 'name': 'log',
+            'operands': ['imm', 'reg', 'unused'] },
+    0xa1: { 'name': 'log',
+            'operands': ['imm', 'unused', 'imm'] },
+    0xb0: { 'name': 'rdsts',
+            'operands': ['reg', 'unused', 'unused'] },
+    0xb1: { 'name': 'unk',
+            'operands': ['reg', 'unused', 'unused'] },
+    0xb2: { 'name': 'rand',
+            'operands': ['reg', 'unused', 'unused'] },
+    0xc0: { 'name': 'sleep',
+            'operands': ['unused', 'unused', 'imm'] },
+    0xc1: { 'name': 'unk1',
+            'operands': ['reg', 'unused', 'unused'] },
+    0xc2: { 'name': 'unk2',
+            'operands': ['unused', 'unused', 'unused'] },
+    0xc3: { 'name': 'unk3',
+            'operands': ['unused', 'unused', 'unused'] },
+    0xff: { 'name': 'end',
+            'operands': ['unused', 'unused', 'unused'] }
+}
+
+class BigScript:
+    def __init__(self, code_string = None, code_bytes = None):
+        global opcodes
+
+        self.instructions = []
+        self.op_dict = opcodes
+
+        if not code_string is None:
+            self.assemble(code_string)
+            self.code_string = code_string
+        elif not code_bytes is None:
+            self.disassemble(code_bytes)
+            self.code_bytes = code_bytes
+
+    def assemble(self, code_string):
+        all_lines = [line.strip() for line in code_string.splitlines()]
+        labels = {}
+
+        # remove labels from code listing and replace it with actual line numbers
+        offset = 0
+        for i in range(0, len(all_lines)):
+            if all_lines[i][0] == '_':
+                labels[all_lines[i]] = i-offset
+                offset+=1
+
+        code_lines = [line for line in all_lines if not line[0] == '_']                
+        code_lines_count = len(code_lines)
+        for i in range(0, code_lines_count):
+            line = code_lines[i]
+            for label in labels:
+                if label in line:
+                    code_lines[i] = line.replace(label, str(labels[label]))
+
+        
+        self.instructions = [Instruction(self.op_dict, i*INSTR_LEN, instr_string=code_lines[i]) for i in range(0, code_lines_count)]
+
+        if not self.instructions[0].is_start():
+            print('WARNING: the input script doesn\'t start with `start` opcode. ACM will reject this script.')
+        if not self.instructions[-1].is_end():
+            print('WARNING: the input script doesn\'t end with `end` opcode. ACM will reject this script.')
+
+        self.code_bytes = b''
+        for instr in self.instructions:
+            self.code_bytes += instr.to_bytes()
+
+    def disassemble(self, code_bytes):
+        code_len = len(code_bytes)
+        if not code_len % INSTR_LEN == 0:
+            raise Exception('ERROR: code size need to be aligned to 8. The input script is corrupted.')
+
+        code_lines_count = code_len // INSTR_LEN
+        code_lines = [code_bytes[i:i + INSTR_LEN] for i in range(0, code_len, INSTR_LEN)]
+        self.instructions = [Instruction(self.op_dict, i*INSTR_LEN, instr_bytes=code_lines[i]) for i in range(0, code_lines_count)]
+
+        if not self.instructions[0].is_start():
+            print('WARNING: the input script doesn\'t start with `start` opcode. It may be corrupted.')
+        if not self.instructions[-1].is_end():
+            print('WARNING: the input script doesn\'t end with `end` opcode. It may be corrupted.')
+
+        labels = []
+        for instr in [instr for instr in self.instructions if instr.is_jump()]:
+            label_addr = instr.get_jump_line()
+            if not label_addr in labels:
+                labels.append(label_addr)
+        
+        code_str_lines = []
+        for instr in self.instructions:
+            if instr.is_jump():
+                code_str_lines.append('\t' + instr.get_opcode_name() + ' ' + '_lb' + str(instr.get_jump_line()))
+            else:
+                code_str_lines.append('\t' + instr.to_string())
+
+        labels.sort()
+        offset = 0
+        for label in labels:
+            code_str_lines.insert(label+offset, '_lb'+str(label))
+            offset+=1
+
+        self.code_str = '\n'.join(code_str_lines)
+
+    def to_string(self):
+        return self.code_str
+
+    def to_bytes(self):
+        return self.code_bytes
+
+
+class Instruction:
+    def __init__(self, opcodes_dict, instr_addr, instr_string = None, instr_bytes = None):
+        self.op_dict = opcodes_dict
+        self.addr = instr_addr
+        self.opcode_name = ""
+        self.opcode_num = None
+        self.operands = [None, None, None]
+
+        if not instr_string is None:
+            self.assemble(instr_string)
+            self.instr_str = instr_string
+        elif not instr_bytes is None:
+            self.disassemble(instr_bytes)
+            self.instr_bytes = instr_bytes
+
+    def assemble(self, instr_string):
+        instr_str_splt = [part.strip() for part in instr_string.split()]
+        self.opcode_name = instr_str_splt[0]
+
+        operands_str = instr_str_splt[1:]
+        operands_count = len(operands_str)
+        used_operands_mask = []
+        for i in range(0, operands_count):
+            op = Operand(self.addr, operands_str[i])
+            used_operands_mask.append(op.get_type())
+            self.operands[i] = op
+
+        # proccessing the case of various opcodes with same names
+        for current_op_num in self.op_dict:
+            opcode = self.op_dict[current_op_num]
+            if opcode['name'] == self.opcode_name:
+                used_ops = [op for op in opcode['operands'] if not op == 'unused']
+                if used_ops == used_operands_mask:
+                    true_operands_mask = opcode['operands']
+                    self.opcode_num = current_op_num
+
+        if self.opcode_num is None:
+            raise Exception('ERROR: unknown opcode ' + self.opcode_name + ' with operands ' + str(used_operands_mask) + ' at ' + hex(self.addr) + '')
+
+        # fix operands numbers
+        for i in range(0, operands_count):
+            operand = self.operands[i]
+
+            if operand.get_type() == true_operands_mask[i]:
+                continue
+
+            for j in range(i, MAX_OPERANDS):
+                if true_operands_mask[j] == operand.get_type():
+                    operand.set_num(j)
+                    tmp = self.operands[j]
+                    self.operands[j] = operand
+                    self.operands[i] = tmp
+
+        for i in range(0, MAX_OPERANDS):
+            if self.operands[i] is None:
+                self.operands[i] = Operand(self.addr, op_num=i)
+                self.operands[i].set_type('unused')
+                self.operands[i].set_value(0)
+
+        self.instr_bytes = struct.pack('HBBI', self.opcode_num, self.operands[0].get_value(), self.operands[1].get_value(), self.operands[2].get_value())
+
+    def disassemble(self, instr_bytes):
+        self.opcode_num, op1, op2, op3 = struct.unpack('HBBI', instr_bytes)
+
+        if not self.opcode_num in self.op_dict:
+            raise Exception('ERROR: unknown opcode ' + hex(self.opcode_num) + ' at ' + hex(self.addr) + '')
+
+        instr_dict = self.op_dict[self.opcode_num]
+        self.operands[OP1_NUM] = Operand(self.addr, op_value=op1, op_num=OP1_NUM, root_instr_dict=instr_dict)
+        self.operands[OP2_NUM] = Operand(self.addr, op_value=op2, op_num=OP2_NUM, root_instr_dict=instr_dict)
+        self.operands[OP3_NUM] = Operand(self.addr, op_value=op3, op_num=OP3_NUM, root_instr_dict=instr_dict)
+        self.opcode_name = self.op_dict[self.opcode_num]['name']
+
+        self.instr_str = self.opcode_name
+        for op in self.operands:
+            if not op.is_unused():
+                self.instr_str += ' ' + op.to_string()
+
+
+    def to_bytes(self):
+        return self.instr_bytes
+
+    def to_string(self):
+        return self.instr_str
+
+    def is_start(self):
+        # @TODO: make it not that ugly
+        return self.opcode_num == 0x1
+
+    def is_end(self):
+        # @TODO: make it not that ugly
+        return self.opcode_num == 0xff
+
+    def is_jump(self):
+        # @TODO: make it not that ugly
+        return self.opcode_name[0] == 'j'
+
+    def get_operand(self, num):
+        return self.operands[num]
+
+    def get_opcode_name(self):
+        return self.opcode_name
+
+    def get_jump_line(self):
+        if self.is_jump():
+            return self.get_operand(OP3_NUM).get_value()
+
+class Operand:
+    def __init__(self, root_instr_addr, op_string = None, op_value = None, op_num = None, root_instr_dict = None):
+        self.instr_dict = root_instr_dict
+        self.instr_addr = root_instr_addr
+        self.op_type = None
+        self.op_str = None
+        self.op_value = op_value
+        self.op_num = op_num
+
+        if not op_string is None:
+            self.assemble(op_string)
+            self.op_str = op_string
+        elif not op_value is None:
+            self.disassemble(op_value)
+            self.op_value = op_value
+
+    def assemble(self, op_string):
+        first_chr = op_string[0]
+        if first_chr == 'f':
+            self.op_type = 'flash'
+        elif first_chr == 'b':
+            self.op_type = 'buff'
+        elif first_chr == 'r':
+            self.op_type = 'reg'
+        elif first_chr.isdigit():
+            self.op_type = 'imm'
+        else:
+            raise Exception('ERROR: unknown type of operand ' + op_string + ' at addr ' + hex(self.instr_addr))
+
+        self.op_value = 0
+        possible_values = re.findall('0[xX][0-9a-fA-F]+|\\d+', op_string)
+        for value_str in possible_values:
+            value_base = 10 + int('0x' in value_str) * 6
+            value = int(value_str, value_base)
+            if not value == 0:
+                self.op_value = value
+                break
+
+    def disassemble(self, op_value):
+        if self.instr_dict is None:
+            raise Exception('ERROR: instruction dict need to be passed for operands dissasembly (addr ' + hex(self.instr_addr) + ')')
+        if self.op_value is None:
+            raise Exception('ERROR: operand value need to be passed for operands dissasembly (addr ' + hex(self.instr_addr) + ')')
+
+        operands_mask = self.instr_dict['operands']
+        self.op_type = operands_mask[self.op_num]
+        if self.op_type == 'flash':
+            self.op_str = 'flash' + str(self.op_value)
+        elif self.op_type == 'buff':
+            self.op_str = 'buffer' + str(self.op_value)
+        elif self.op_type == 'reg':
+            self.op_str = 'r' + str(self.op_value)
+        elif self.op_type == 'imm':
+            self.op_str = hex(self.op_value)
+
+    def get_type(self):
+        return self.op_type
+
+    def get_num(self):
+        return self.op_num
+
+    def get_value(self):
+        return self.op_value
+
+    def set_num(self, num):
+        self.op_num = num
+
+    def set_type(self, optype):
+        self.op_type = optype
+
+    def set_value(self, value):
+        self.op_value = value
+
+    def to_string(self):
+        return self.op_str
+
+    def to_bytes(self):
+        if self.op_num == OP3_NUM:
+            return struct.pack('<I', self.op_value)
+        else:
+            return struct.pack('B', self.op_value)
+
+    def is_unused(self):
+        return self.op_type == 'unused'
+
+
+def main():  
+    option_list = [
+        make_option('-a', '--assemble', dest = 'asm', action = 'store', default = None,
+            help = 'assemble a BIOS Guard script source code file'), 
+
+        make_option('-d', '--disassemble', dest = 'disasm', action = 'store', default = None,
+            help = 'disassemble a BIOS Guard script binary file')
+    ]
+    parser = OptionParser(option_list = option_list)
+    (options, args) = parser.parse_args()
+
+
+    if not options.asm is None:
+        with open(options.asm) as f:
+            src = f.read()
+
+        script = BigScript(code_string=src)
+        out_file = options.asm + '_assembled.bin'
+        with open(out_file, 'wb') as f:
+            f.write(script.to_bytes())
+
+        print('SUCCESS: assembled code written to ' + out_file)
+
+        return 0
+
+    elif not options.disasm is None:
+        with open(options.disasm, 'rb') as f:
+            binary = f.read()
+        
+
+        script = BigScript(code_bytes=binary)
+        out_file = options.disasm + '_disassembled.txt'
+        with open(out_file, 'w') as f:
+            f.write(script.to_string())
+
+        print('SUCCESS: disassembled code written to ' + out_file)
+        return 0
+    else:
+        print('[!] No actions specified, try --help')
+        return -1
+
+
+if __name__ == '__main__':
+    exit(main())
